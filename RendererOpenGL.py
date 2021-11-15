93ecc99037503d4db02b028d40cc4a5e722f7ba0
Christian Pérez <christiandp@live.com>
2021-11-15 12:40:21 -0600

    Cámara implementada

--- RendererOpenGL.py
+++ RendererOpenGL.py
@@ -2,36 +2,92 @@ import pygame
 from gl import Renderer, Model
 import numpy as np
 import shaders
+from pygame.locals import *
 
 width = 960
 height = 540
 
+deltaTime = 0.0
+
 pygame.init()
 screen = pygame.display.set_mode(
     (width, height), pygame.DOUBLEBUF | pygame.OPENGL)
-
 clock = pygame.time.Clock()
 
 rend = Renderer(screen)
 rend.setShaders(shaders.vertex_shader, shaders.fragment_shader)
-vertex_data = np.array([-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-                        0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-                        0.0, 0.5, 0.0, 0.0, 0.0, 1.0], dtype=np.float32)
 
-rend.scene.append(Model(vertex_data))
+vertex_data = np.array([-0.5, -0.5, 0.5, 1.0, 0.0, 0.0,
+                        -0.5, 0.5, 0.5, 0.0, 1.0, 0.0,
+                        0.5, 0.5, 0.5, 0.0, 0.0, 1.0,
+                        0.5, -0.5, 0.5, 1.0, 1.0, 0.0,
+                        -0.5, -0.5, -0.5, 1.0, 0.0, 1.0,
+                        -0.5, 0.5, -0.5, 0.0, 1.0, 1.0,
+                        0.5, 0.5, -0.5, 1.0, 1.0, 1.0,
+                        0.5, -0.5, -0.5, 0.0, 0.0, 0.0], dtype=np.float32)
+
+index_data = np.array([0, 1, 3, 1, 2, 3,
+                       1, 5, 2, 5, 6, 2,
+                       4, 5, 0, 5, 1, 0,
+                       3, 2, 7, 6, 2, 7,
+                       4, 0, 7, 0, 3, 7,
+                       5, 4, 6, 4, 7, 6], dtype=np.uint32)
+
+cube = Model(vertex_data, index_data)
+
+cube.position.z = -5.0
+
+rend.scene.append(cube)
+
 
 isRunning = True
 while isRunning:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+
+    keys = pygame.key.get_pressed()
+
+    # Traslacion de camara
+    if keys[K_d]:
+        rend.camPosition.x += 1 * deltaTime
+    if keys[K_a]:
+        rend.camPosition.x -= 1 * deltaTime
+    if keys[K_w]:
+        rend.camPosition.z += 1 * deltaTime
+    if keys[K_s]:
+        rend.camPosition.z -= 1 * deltaTime
+    if keys[K_q]:
+        rend.camPosition.y -= 1 * deltaTime
+    if keys[K_e]:
+        rend.camPosition.y += 1 * deltaTime
+
+    # Rotacion de camara
+    if keys[K_z]:
+        rend.camRotation.y += 15 * deltaTime
+    if keys[K_x]:
+        rend.camRotation.y -= 15 * deltaTime
+
+    # Movimiento de objeto
+    rend.scene[0].rotation.x += 10 * deltaTime
+    rend.scene[0].rotation.y += 10 * deltaTime
+    rend.scene[0].rotation.z += 10 * deltaTime
+
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
             isRunning = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+
+        elif ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_ESCAPE:
                 isRunning = False
+
+            if ev.key == K_1:
+                rend.filledMode()
+            if ev.key == K_2:
+                rend.wireframeMode()
+
+    rend.tiempo += deltaTime
+    deltaTime = clock.tick(60) / 1000
 
     rend.render()
 
-    clock.tick(60)
     pygame.display.flip()
 
 pygame.quit()
--- shaders.py
+++ shaders.py
@@ -4,10 +4,15 @@ vertex_shader = """
 #version 460
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 inColor;
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+uniform float tiempo;
 out vec3 outColor;
 void main()
 {
-    gl_Position = vec4(position.x, position.y, position.z, 1.0);
+    vec4 pos = vec4(position.x, position.y, position.z, 1.0);
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * pos;
     outColor = inColor;
 }
 """
